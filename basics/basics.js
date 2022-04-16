bf05c52642e131c6815e02bb031392327a704004
Vinesse <vverning@gmail.com>
2022-04-16 23:12:42 +0200

    subdir for obj practice

--- basics/basics.js
+++ basics/basics.js
@@ -40,3 +40,11 @@ function myFunction(str) {
     return str.slice(-3);
 }
 
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+
+function myFunction(str) {
+    return str.slice(0, 3);
+}
+
