ca22a3ee358b97e60df200c38a267b6582e0cf2d
Vinesse <vverning@gmail.com>
2022-04-16 22:15:45 +0200

    basic functions

---
+++ basics/basics.js
@@ -0,0 +1,18 @@
+// Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result
+
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a, b) {
+    return a == b && typeof a == typeof b;
+    // alternatively,
+    // return a === b; 
+    // would have worked.
+    // three 'equals' === checked for strict equality including type
+}
