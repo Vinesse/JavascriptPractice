5cb2cc88458e0a223935659d7ccade46126dc1c1
Vinesse <vverning@gmail.com>
2022-04-17 22:27:41 +0200

    accessing object properties

--- objects/objects.js
+++ objects/objects.js
@@ -46,4 +46,43 @@ const fictionalCharacter = {
 
 }
 
-// question: can objects be consts????
+// Question: Can Objects be Const?
+// Answer: Const works with Objects and Arrays.
+// This prevents Objects and Arrays from being overwriten entirely.
+// However, this does not protect object keys. The same applies to arrays.
+
+// Here we want to access and update properties nested within objects
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+
+// To set the value of the online key to 45:
+userActivity.data.online = 45;
+
+// Accessing property names with Bracket Notation
+// We've defined a function, checkInventory, which receives a scanned item as an argument.
+// Return the current value of the scannedItem key in the foods object.
+// You can assume that only valid keys will be provided as an argument to checkInventory.
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+
+function checkInventory(scannedItem) {
+    return foods[scannedItem];
+// This takes the value of scanned item, and checks it against foods which returns the value of the key.
+// Considering the console.log below, it would pass 'apples' and check for it against the foods objects
+// Which would return the value 27.
+}
+
+console.log(checkInventory("apples"));
